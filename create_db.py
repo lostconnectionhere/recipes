9e8631189e0c3605c8a5eb828cad0209b88c61c1
Roz <rozsabir1@gmail.com>
2023-01-26 20:28:11 +0100

    created three tables to fill with data

--- create_db.py
+++ create_db.py
@@ -13,18 +13,18 @@ mycursor = mydb.cursor()
 mycursor.execute("""
     CREATE TABLE IF NOT EXISTS recipes (
         recipe_id INT AUTO_INCREMENT PRIMARY KEY, 
-        recipe_name_EN VARCHAR(255), 
+        recipe_name_EN VARCHAR(255) NOT NULL, 
         recipe_name_KU VARCHAR(255), 
-        total_time VARCHAR(255), 
-        directions VARCHAR(255), 
+        total_time INT, 
+        directions LONGTEXT NOT NULL, 
         author VARCHAR(255)
     )""")
 
 #create table ingredients
 mycursor.execute("""
     CREATE TABLE IF NOT EXISTS ingredients (
-        ingredient_id INT PRIMARY KEY, 
-        ingredient_name VARCHAR(255)
+        ingredient_id INT AUTO_INCREMENT PRIMARY KEY, 
+        ingredient_name VARCHAR(255) NOT NULL
     )""")
 
 #create table recipe_ingredients
@@ -32,18 +32,14 @@ mycursor.execute("""
     CREATE TABLE IF NOT EXISTS recipe_ingredients (
         recipe_id INT, 
         ingredient_id INT,
-        measurement_unit ENUM('g', 'kg', 'tsp'. 'tbsp', 'ml', 'l', 'cup', 'piece(s)'),
-        amount INT
+        measurement_unit ENUM('g', 'kg', 'tsp', 'tbsp', 'ml', 'l', 'cup', 'piece(s)') NOT NULL,
+        amount INT NOT NULL,
         FOREIGN KEY (recipe_id) REFERENCES recipes(recipe_id), 
         FOREIGN KEY (ingredient_id) REFERENCES ingredients(ingredient_id)
 
     )""")
 
-# mycursor.execute("SHOW TABLES")
-mycursor.execute("SELECT * FROM measurement_qty")
+mycursor.execute("SHOW TABLES")
 
-myresult = mycursor.fetchall()
-
-for x in myresult:
+for x in mycursor:
   print(x)
-
