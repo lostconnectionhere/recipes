1fa42447a298a6390de879f27743e7df53d997e2
Roz <rozsabir1@gmail.com>
2023-02-20 15:07:29 +0100

    trying to fix links and add_ingredient

--- Flask/main.py
+++ Flask/main.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/about/')
-def about():
-    return '<h3>This is a Flask web application.</h3>'
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def main_menu():
+    return render_template('main_menu.html')
+
+# /main_menu/recipe_menu <-- geen .html
+@app.route('/main_menu/recipe_menu') # denk ja 
+def recipe_menu():
+    return render_template('recipe_menu.html')
+
+@app.route('/main_menu/ingredient_menu')
+def ingredient_menu():
+    return render_template('ingredient_menu.html')
+
+@app.route('/recipe/<recipe_name>') #test
+def get_recipe(recipe):
+    return "The recipe is " +str(recipe)
 
 app.run(host='0.0.0.0',port=8001)
 
+
+# https://pythonbasics.org/flask-tutorial-routes/
+# http://localhost:8001/main_menu/recipe_menu <-- geen .html...     
+    
