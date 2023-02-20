373bdcf36fc9382cf4bbd9b9a227c9272bea2a1b
Roz <rozsabir1@gmail.com>
2023-02-20 15:45:45 +0100

    show ingredients_list

--- Flask/main.py
+++ Flask/main.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "my-secret-pw",
+    database = "mydb_test"
+)
+
+mycursor = mydb.cursor()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,7 +17,7 @@ def main_menu():
     return render_template('main_menu.html')
 
 # /main_menu/recipe_menu <-- geen .html
-@app.route('/main_menu/recipe_menu') # denk ja 
+@app.route('/main_menu/recipe_menu') 
 def recipe_menu():
     return render_template('recipe_menu.html')
 
@@ -14,13 +25,22 @@ def recipe_menu():
 def ingredient_menu():
     return render_template('ingredient_menu.html')
 
-@app.route('/recipe/<recipe_name>') #test
-def get_recipe(recipe):
-    return "The recipe is " +str(recipe)
+@app.route('/add_ingredient')
+def add_data():
+    return render_template('add_ingredient.html')
+
+@app.route('/list_ingredients')
+def list_ingredients():
+    print("Here you can see all the ingredients: ")
+    mycursor.execute("SELECT ingredient_id, ingredient_name FROM ingredients")
+    result_ing = mycursor.fetchall()
+
+
+# @app.route('/recipe/<recipe_name>') #test
+# def get_recipe(recipe):
+#     return "The recipe is " +str(recipe)
 
 app.run(host='0.0.0.0',port=8001)
 
 
 # https://pythonbasics.org/flask-tutorial-routes/
-# http://localhost:8001/main_menu/recipe_menu <-- geen .html...     
-    
