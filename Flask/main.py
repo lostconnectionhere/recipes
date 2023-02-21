9bbaeab30f0f9be73df08b91ab4bf77c90ecf4d9
Roz <rozsabir1@gmail.com>
2023-02-21 19:17:28 +0100

    CRUD done for ingredients table

--- Flask/main.py
+++ Flask/main.py
@@ -16,30 +16,35 @@ app = Flask(__name__)
 def main_menu():
     return render_template('main_menu.html')
 
-# /main_menu/recipe_menu <-- geen .html
 @app.route('/main_menu/recipe_menu') 
 def recipe_menu():
     return render_template('recipe_menu.html')
 
+# Ingredient Menu CRUD
 @app.route('/main_menu/ingredient_menu')
 def ingredient_menu():
     return render_template('ingredient_menu.html')
 
-@app.route('/update_ingredient')
-def update_ingredient(old_ing_name, ingredient_id):
-    mycursor.execute("UPDATE ingredients SET ingredient_name = %s WHERE ingredient_id = %s" , (old_ing_name, ingredient_id))
-    mydb.commit()
-
-    return render_template('add_ingredient.html')
-
+# 1.1 Show Ingredients List
 @app.route('/list_ingredients')
 def list_ingredients():
     mycursor.execute("SELECT ingredient_id, ingredient_name FROM ingredients")
     result_ing = mycursor.fetchall()
-    # for ingredient in result_ing:
-    #     print(ingredient)
     return render_template("list_ingredients.html", value = result_ing)
 
+# 1.2 Add Ingredient 
+@app.route('/add_ingredient')
+def add_ing_screen():
+    return render_template('add_ingredient.html')
+
+@app.route('/add_ingredient/add', methods=['POST']) 
+def add_ingredient():
+    ingredient_name = request.form['ingredient_name']
+    mycursor.execute("INSERT INTO ingredients (ingredient_name) VALUES (%s)", (ingredient_name,))
+    mydb.commit()
+    return redirect(url_for('list_ingredients'))
+
+# 1.3 Delete Ingredient
 @app.route('/delete_ingredient')
 def delete_ing_screen():
     return render_template('delete_ingredient.html')
@@ -50,18 +55,19 @@ def delete_ingredient(ingredient_id):
     mydb.commit()
     return redirect(url_for('list_ingredients'))
 
-@app.route('/add_ingredient')
-def add_ing_screen():
-    return render_template('add_ingredient.html')
+# 1.4 Update Ingredient
+@app.route('/update_ingredient')
+def update_ing_screen():
+    return render_template('update_ingredient.html')
 
-@app.route('/add_ingredient/do', methods=['POST']) 
-def foo():
-        ingredient_name = request.form['ingredient_name']
-        mycursor.execute("INSERT INTO ingredients (ingredient_name) VALUES (%s)", (ingredient_name,))
-        mydb.commit()
-        return redirect(url_for('list_ingredients'))
+@app.route('/update_ingredient/update', methods=['POST'])
+def update_ingredient():
 
-
+    ingredient_id = request.form['ingredient_id']
+    ingredient_name = request.form['ingredient_name']
+    mycursor.execute("UPDATE ingredients SET ingredient_name = %s WHERE ingredient_id = %s" , (ingredient_name, ingredient_id))
+    mydb.commit()
+    return redirect(url_for('list_ingredients'))
 
 
 app.run(host='0.0.0.0',port=8001)
