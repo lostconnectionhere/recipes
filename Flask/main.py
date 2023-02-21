b29f0d1b6e20b74920a35da354db96b807346322
Roz <rozsabir1@gmail.com>
2023-02-21 17:00:45 +0100

    fixed insert

--- Flask/main.py
+++ Flask/main.py
@@ -27,20 +27,13 @@ def ingredient_menu():
 
 @app.route('/update_ingredient')
 def update_ingredient(old_ing_name, ingredient_id):
-    # old_ing_id = input("Which ingredient do you want to update? ")
-    # new_ing_name = input("Sure, to what ingredient do you want to update it to? ")
-
-    mycursor.execute("UPDATE ingredients SET ingredient_name = %s WHERE ingredient_id = %s" % (old_ing_name, ingredient_id))
+    mycursor.execute("UPDATE ingredients SET ingredient_name = %s WHERE ingredient_id = %s" , (old_ing_name, ingredient_id))
     mydb.commit()
 
     return render_template('add_ingredient.html')
 
 @app.route('/list_ingredients')
 def list_ingredients():
-    # Probeer de list_ingredients functie te gebruiken die je al had. 
-    # laat die result_ing die je nu hier hebt staan return'en. e.g.:
-    # result = list_ingredients()
-    # render_template(..., value = result);  
     mycursor.execute("SELECT ingredient_id, ingredient_name FROM ingredients")
     result_ing = mycursor.fetchall()
     # for ingredient in result_ing:
@@ -53,26 +46,37 @@ def delete_ing_screen():
 
 @app.route('/delete_ingredient/<int:ingredient_id>')
 def delete_ingredient(ingredient_id):
-    mycursor.execute("DELETE FROM ingredients WHERE ingredient_id = %s" % (ingredient_id,))
+    mycursor.execute("DELETE FROM ingredients WHERE ingredient_id = %s" , (ingredient_id,))
     mydb.commit()
     return redirect(url_for('list_ingredients'))
 
+
+@app.route('/add_ingredient/do', methods=['POST']) 
+def foo():
+        ingredient_name = request.form['ingredient_name']
+        mycursor.execute("INSERT INTO ingredients (ingredient_name) VALUES (%s)", (ingredient_name,))
+        mydb.commit()
+        return redirect(url_for('list_ingredients'))
+
+
+# @app.route('/add_ingredient/do', methods = ['POST'])
+# def insert():
+#     if request.method == "POST":
+#         ingredient_name = request.form['ingredient_name']
+#         mycursor.execute("INSERT INTO ingredients (ingredient_name) VALUES (%s)", (ingredient_name,))
+#         mydb.commit()
+#         return redirect(url_for('list_ingredients'))
 
 @app.route('/add_ingredient')
 def add_ing_screen():
     return render_template('add_ingredient.html')
 
-@app.route('/add_ingredient', methods=['POST'])
-def add_ingredient():
-    data = request.get_json
-    insert_query = ("INSERT INTO ingredients (ingredient_name) VALUES (?)")
-    value = (data['ingredient_name'])
-    mycursor.execute(insert_query, value)
-    # ingredient_name = request.json
-    mydb.commit()
-    return jsonify({'message': 'Ingredient added successfuly.'})
-    # return redirect(url_for('list_ingredients'))
-
+# @app.route('/add_ingredient/<string:ingredient_name>')
+# def add_ingredient(ingredient_name):
+#     mycursor.execute("INSERT INTO ingredients (ingredient_name) VALUES (?)" % (ingredient_name,))
+#     mydb.commit()
+#     print("insert done")
+#     return redirect(url_for('list_ingredients'))
 
 
 app.run(host='0.0.0.0',port=8001)
