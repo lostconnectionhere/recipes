589b943c83897c1e8a56425593c922a13c7642d1
Roz <rozsabir1@gmail.com>
2023-02-21 23:11:38 +0100

    working on seeding

--- seed_tables1702.py
+++ seed_tables1702.py
@@ -8,6 +8,14 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+# recipe_name = input("From which recipe do you want to know the id? Type in the recipe name ")
+# print(type(recipe_name))
+# print(recipe_name)
+# sql = "SELECT recipe_id FROM recipes WHERE recipe_name_EN = " + recipe_name
+id = mycursor.execute("SELECT recipe_id FROM recipes WHERE recipe_name_EN = 'Kurdish Naan Bread'")
+mydb.commit()
+print(id)
+
 
 # STEP 1. Cleanup tables
 # STEP 1.1 Cleanup tables with auto-increment
@@ -30,44 +38,44 @@ mycursor = mydb.cursor()
 
 # STEP 2. Insert values in tables
 # STEP 2.1 Insert recipes (seed)
-sql_recipes = "INSERT INTO recipes (recipe_name_EN, recipe_name_KU, total_time, directions, author) VALUES (%s, %s, %s, %s, %s)"
-val_recipes = [["Kurdish Naan Bread", "Naan", 180, "Step 1: Mix a little bit of flour with the tsp of sugar, warm water and baking powder. Let this rest for a few minutes. ..", "Nasrin Faraj"], 
-               ["Minced Meat with Herbs", "Shifta", 60, "Step 1: Get minced meat of your choice (e.g. chicken or lamb), slice fresh parsley, garlic and onion. Step 2: Mix the minced meat with ..", "Roz Sabir"],
-               ["Mixed Vegetables in Oven", "Tapsi", 60, "Step 1: Cut your zucchini, eggplant, onions, potatoes and tomatoes in round slices. Step 2: Fry the vegetabels in sunflower oil ..", "Nasrin Faraj"],
-               ["Lentil Soup", "Nesqena", 30, "Step 1: Rinse the red lentils under cold running water. Cut the onion and garlic in very small pieces ..", "Nasrin Faraj"],
-               ["Easy Fresh Salad", "Zalata", 10, "Step 1: Slice the cucumber and tomato in equal size cubes, the smaller the better. Step 2: Cut the onion is slices", "Roz Sabir"]
-               ]
-mycursor.executemany(sql_recipes, val_recipes)
+# sql_recipes = "INSERT INTO recipes (recipe_name_EN, recipe_name_KU, total_time, directions, author) VALUES (%s, %s, %s, %s, %s)"
+# val_recipes = [["Kurdish Naan Bread", "Naan", 180, "Step 1: Mix a little bit of flour with the tsp of sugar, warm water and baking powder. Let this rest for a few minutes. ..", "Nasrin Faraj"], 
+#                ["Minced Meat with Herbs", "Shifta", 60, "Step 1: Get minced meat of your choice (e.g. chicken or lamb), slice fresh parsley, garlic and onion. Step 2: Mix the minced meat with ..", "Roz Sabir"],
+#                ["Mixed Vegetables in Oven", "Tapsi", 60, "Step 1: Cut your zucchini, eggplant, onions, potatoes and tomatoes in round slices. Step 2: Fry the vegetabels in sunflower oil ..", "Nasrin Faraj"],
+#                ["Lentil Soup", "Nesqena", 30, "Step 1: Rinse the red lentils under cold running water. Cut the onion and garlic in very small pieces ..", "Nasrin Faraj"],
+#                ["Easy Fresh Salad", "Zalata", 10, "Step 1: Slice the cucumber and tomato in equal size cubes, the smaller the better. Step 2: Cut the onion is slices", "Roz Sabir"]
+#                ]
+# mycursor.executemany(sql_recipes, val_recipes)
 
-mydb.commit()
+# mydb.commit()
 
-print(mycursor.rowcount, "record(s) inserted for table recipes")
+# print(mycursor.rowcount, "record(s) inserted for table recipes")
 
-# STEP 2.2 Insert ingredients (seed)
-sql_ingredients = "INSERT INTO ingredients (ingredient_name) VALUES (%s)"
-val_ingredients = [
-    ["baking powder"],
-    ["salt"],
-    ["egg"],
-    ["sunflower oil"],
-    ["water"],
-    ["all purpose flower"],
-    ["sugar"],
-    ["zucchini"],
-    ["eggplant"],
-    ["tomato"],
-    ["garlic"],
-    ["onion"],
-    ["sumac"],
-    ["potatoes"],
-    ["cumin powder"]
-]
+# # STEP 2.2 Insert ingredients (seed)
+# sql_ingredients = "INSERT INTO ingredients (ingredient_name) VALUES (%s)"
+# val_ingredients = [
+#     ["baking powder"],
+#     ["salt"],
+#     ["egg"],
+#     ["sunflower oil"],
+#     ["water"],
+#     ["all purpose flower"],
+#     ["sugar"],
+#     ["zucchini"],
+#     ["eggplant"],
+#     ["tomato"],
+#     ["garlic"],
+#     ["onion"],
+#     ["sumac"],
+#     ["potatoes"],
+#     ["cumin powder"]
+# ]
 
-mycursor.executemany(sql_ingredients, val_ingredients)
+# mycursor.executemany(sql_ingredients, val_ingredients)
 
-mydb.commit()
+# mydb.commit()
 
-print(mycursor.rowcount, "record(s) inserted for table ingredients: ")
+# print(mycursor.rowcount, "record(s) inserted for table ingredients: ")
 
 # STEP 2.3 Insert recipe_ingredients (seed)
 # sql_recipe_ingredient = "INSERT INTO recipe_ingredients (recipe_id, ingredient_id, measurement_unit, amount) VALUES (%s, %s, %s, %s)"
